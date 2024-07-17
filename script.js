5f2f9ed4eca61623b34e74f78767be50ee671cd3
Mararovsek99 <andrej.marovsek99@gmail.com>
2024-07-17 21:43:25 +0200

    added popup window for add book

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const myLibrary = [];
+const myLibrary = [{
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    pages: 281,
+    read: "read",
+},
+{
+    title: "1984",
+    author: "George Orwell",
+    pages: 328,
+    read: "read",
+},
+{
+    title: "Moby Dick",
+    author: "Herman Melville",
+    pages: 635,
+    read: "read",
+},
+{
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    pages: 180,
+    read: "read",
+}];
+
+
+
+
+
+
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -7,16 +36,55 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-function addBookToLibrary(){
+function displayEachBook(){
 
-    let title = prompt("Enter the title of the book");
-    let author = prompt("Enter the author of the book");
-    let pages = prompt("Enter the number of pages");
-    let read = prompt("Have you read this book?");
 
-    const newBook = new Book(title, author, pages, read);
 
-    myLibrary.push(newBook);
+    myLibrary.forEach(book => {
 
-    console.log(`${title} by ${author} has been added to the library`);
+    const container = document.querySelector(".main-page");
+
+    let newCard = document.createElement("div");
+    newCard.className = "card";
+    container.appendChild(newCard);
+    
+    let newTitle = document.createElement("h1");
+    newTitle.textContent = book.title;
+    newCard.appendChild(newTitle);
+
+    let newAuthor = document.createElement("h2");
+    newAuthor.textContent = book.author;
+    newCard.appendChild(newAuthor);
+
+    let newPages = document.createElement("h2");
+    newPages.textContent = book.pages;
+    newCard.appendChild(newPages);
+
+    let newRead = document.createElement("button");
+        newRead.className = "read";
+        newRead.textContent = book.read;
+    newCard.appendChild(newRead);
+
+    let newDel = document.createElement("button");
+    newDel.textContent = "Remove";
+newCard.appendChild(newDel);
+    
+        
+    });
+
+    
+}
+
+
+
+
+const addBookBtn = document.querySelector("#addBookBtn");
+addBookBtn.addEventListener("click", function(){
+    openDialog();
+})
+function openDialog(){
+    const dialog = document.querySelector("dialog");
+    const overlay = document.querySelector("#overlay");
+    dialog.showModal();
+    overlay.style.display = "block";
 }
