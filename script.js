ee23e45f03b68331007c001d89366e9e41cb09f4
Mararovsek99 <andrej.marovsek99@gmail.com>
2024-07-18 22:49:25 +0200

    added js:add new book to array and add new book on web page

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-const myLibrary = [{
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    pages: 281,
-    read: "read",
-},
-{
-    title: "1984",
-    author: "George Orwell",
-    pages: 328,
-    read: "read",
-},
-{
-    title: "Moby Dick",
-    author: "Herman Melville",
-    pages: 635,
-    read: "read",
-},
-{
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    pages: 180,
-    read: "read",
-}];
+const myLibrary = [];
 
 
 
@@ -95,5 +72,61 @@ submitAddBookBtn.addEventListener("click", function(){
     const overlay = document.querySelector("#overlay");
     dialog.close();
     overlay.style.display = "none";
+
+})
+
+document.getElementById("submitAddBookBtn").addEventListener("click", function(){
+
+    let form = document.getElementById("addBookForm");
+
+    let formData = new FormData(form);
+
+    let title = formData.get("title");
+    let author = formData.get("author");
+    let pages = formData.get("pages");
+    let read = document.getElementById("checkBoxRead").checked;
+
+
+    let newBook = {
+        title: title,
+        author: author,
+        pages: pages,
+        read: read ? "read" : "not read"
+    }
+    myLibrary.push(newBook);
+
+    const container = document.querySelector(".main-page");
+
+    let newCard = document.createElement("div");
+    newCard.className = "card";
+    container.appendChild(newCard);
+    
+    let newTitle = document.createElement("h1");
+    newTitle.textContent = newBook.title;
+    newCard.appendChild(newTitle);
+
+    let newAuthor = document.createElement("h2");
+    newAuthor.textContent = newBook.author;
+    newCard.appendChild(newAuthor);
+
+    let newPages = document.createElement("h2");
+    newPages.textContent = newBook.pages;
+    newCard.appendChild(newPages);
+
+    let newRead = document.createElement("button");
+        newRead.className = "read";
+        newRead.textContent = newBook.read;
+    newCard.appendChild(newRead);
+
+    let newDel = document.createElement("button");
+    newDel.textContent = "Remove";
+newCard.appendChild(newDel);
+    
+
+
+
+
+    form.reset();
+
 
 })
